9f0cc489f4547bf74755060add05064838eabc22
vilmarvalval <ville.valtasalo@metropolia.fi>
2026-02-11 14:23:22 +0200

    [iter2] Implement job fetching on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,11 +4,24 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [jobs, setJobs] = useState([]);
 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch("/api/jobs");
+        const jobData = await response.json();
+        setJobs(jobData);
+      } catch (error) {
+        console.error("Error fetching jobs:", error);
+      }
+    };
+    fetchJobs();
+  }, []);
+
   return (
     <div className="home">
       <div className="job-list">
         {jobs.length === 0 && <p>No jobs found</p>}
-        {jobs.length !== 0 &&
+        {jobs.length !== 0 && 
           jobs.map((job) => <JobListing key={job.id} {...job} />)}
       </div>
     </div>
